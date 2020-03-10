4cc0a46f91ed9220a42a9ced58022ac7f431e794
Jan Wilczek <janekw@vp.pl>
2020-03-10 10:09:51 +0100

    Minor file changes.

--- Code/main.py
+++ Code/main.py
@@ -9,8 +9,7 @@ def main():
     N = 1000
 
     finish_index = 4 * rate # stop after 4 seconds
-    nlms_signal_error, estimated_impulse_response = NLMS(signal_loudspeaker, signal_microphone, N, 0.5, 0.000001, freeze_index=finish_index)
-    signal_error = nlms_signal_error
+    signal_error, estimated_impulse_response = NLMS(signal_loudspeaker, signal_microphone, N, 0.5, 0.000001, freeze_index=finish_index)
 
     plot_signals(signal_microphone, signal_loudspeaker, impulse_response, signal_error, estimated_impulse_response, N)
 
