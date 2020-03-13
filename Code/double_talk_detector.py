592a228b92b4f9a1a7586b25e53b9bb477f3f5bc
Jan Wilczek <janekw@vp.pl>
2020-03-13 13:48:36 +0100

    WIP: Half of algorithm corrected.

--- Code/double_talk_detector.py
+++ Code/double_talk_detector.py
@@ -2,7 +2,7 @@ from collections import deque
 import numpy as np
 
 def dft_matrix(size):
-    F = np.zeros((size, size))
+    F = np.zeros((size, size), dtype=complex)
     for nu in range(0, size):
         for n in range(0, size):
             F[nu, n] = np.exp(- 1j * 2 * np.pi * nu * n / size)
@@ -19,22 +19,24 @@ class DoubleTalkDetector:
         self.N = N
         self.F_2N = dft_matrix(2 * N)
         self.L = L
-        assert L // N == L / N % "Filter's length is not divisible by block's length!"
-        self.K = L / N
+        assert L // N == L / N, "Filter's length is not divisible by block's length!"
+        self.K = L // N
         self.x_k = deque()   # buffered sample blocks
         self.X_k = deque()   # buffered DFTs of sample blocks as matrices' diagonals
         for k in range(0, self.K):
             self.x_k.append(np.zeros((N, )))
-            self.X_k.append(np.zeros((self.K, self.K)))
-        self.S_prim = np.zeros((2*N, 2*N))
+            self.X_k.append(np.zeros((2 * self.N, 2 * self.N), dtype=complex))
+        self.S_prim = 0.0005 * np.eye(2*N * self.K, dtype=complex) # regularized to make S_prim invertible in the first iteration
         self.W_1 = np.zeros((2*N, 2*N))
-        self.W_1[N:2*N-1, N:2*N-1] = np.eye(N)
+        self.W_1[N:2*N, N:2*N] = np.eye(N)
         self.W_2 = np.zeros((2*N, 2*N))
-        self.W_2[0:N-1, 0:N-1] = np.eye(N)
+        self.W_2[0:N, 0:N] = np.eye(N)
         self.G_1 = self.F_2N @ self.W_1 @ np.linalg.inv(self.F_2N)
         self.G_2_tilde = self.F_2N @ self.W_2 @ np.linalg.inv(self.F_2N)
-        self.G_2 = np.diag(self.G_2_tilde) # How many G_2_tildes???
-        self.h_b = np.zeros((L, ))
+        self.G_2 = np.zeros((2*L, 2*L), dtype=complex)
+        for k in range(0, self.K):
+            self.G_2[k*2*N:(k+1)*2*N,k*2*N:(k+1)*2*N] = self.G_2_tilde # place G_2_tilde along the diagonal of G_2
+        self.h_b = np.zeros((2*L, ))
         self.s_k = []
         for k in range(0, self.K):
             self.s_k = np.zeros((2 * N,))
@@ -43,13 +45,15 @@ class DoubleTalkDetector:
     def enqueue_loudspeaker_block(self, new_samples_block):
         self.x_k.appendleft(new_samples_block)
         self.x_k.pop()
-        assert len(self.x_k) == self.K % f"Inappriopriate number of buffered blocks! Is {len(self.x_k)}, should be {self.K}."
+        assert len(self.x_k) == self.K, f"Inappriopriate number of buffered blocks! Is {len(self.x_k)}, should be {self.K}."
 
-        self.X_k.appendleft(np.diag(self.F_2N @ new_samples_block))
+        x_2N = np.vstack((self.x_k[0], new_samples_block))
+        assert len(x_2N) == 2 * self.N
+        self.X_k.appendleft(np.diag(self.F_2N @ x_2N))
         self.X_k.pop()
 
     def X(self):
-        X = np.zeros((2 * self.N, 2 * self.N * self.K))
+        X = np.zeros((2 * self.N, 2 * self.N * self.K), dtype=complex)
         for k in range(0, self.K):
             X[:, k * 2 * self.N:(k+1) * 2 * self.N] = self.X_k[k]
         return X
@@ -61,21 +65,23 @@ class DoubleTalkDetector:
         self.S_prim = self.lambd * self.S_prim  + (1 - self.lambd) * hermitian(X) @ X
         kalman_gain = np.linalg.inv(self.S_prim) @ hermitian(X)
 
-        y_ = self.F_2N @ np.vstack((np.zeros((self.N,)), microphone_samples_block))
+        zeros_y = np.vstack((np.zeros((self.N, 1)), microphone_samples_block))
+        y_ = self.F_2N @ zeros_y
 
-        error_b = y_ - self.G_1 @ X @ self.h_b
+        background_y_estimate = self.G_1 @ X @ self.h_b
+        error_b = np.subtract(y_.reshape(-1), background_y_estimate)
 
         self.h_b = self.h_b + 2 * (1 - self.lambd_b) * self.G_2 @ kalman_gain @ error_b
 
         for k in range(0, self.K):
-            self.s_k[k] = self.lambd_b @ self.s_k[k] + (1 - self.lambd_b) @ np.conj(self.X_k[k]) @ y_
+            self.s_k[k] = self.lambd_b * self.s_k[k] + (1 - self.lambd_b) * np.conj(self.X_k[k]) @ y_
 
         self.var2_y = self.lambd_b * self.var2_y + (1 - self.lambd_b) * np.dot(hermitian(y_), y_)
 
-        dzeta_sq = sum([np.dot(hermitian(self.h_b[N*k:N*(k+1) - 1]), s_k[k] for k in range(0, self.K)]) / self.var2_y
+        dzeta_sq = sum([np.dot(hermitian(self.h_b[self.N*k:self.N*(k+1) - 1]), self.s_k[k]) for k in range(0, self.K)]) / self.var2_y
         dzeta = np.sqrt(dzeta_sq)
 
-        if dzeta < threshold:
+        if dzeta < self.threshold:
             return True
         else:
             return False
--- Code/main.py
+++ Code/main.py
@@ -6,7 +6,7 @@ from fdaf import NLMS
 def main():
     signal_microphone, signal_loudspeaker, impulse_response, rate = generate_signals()
 
-    N = 1000
+    N = len(impulse_response)
 
     finish_index = 4 * rate # stop after 4 seconds
     signal_error, estimated_impulse_response = NLMS(signal_loudspeaker, signal_microphone, N, 0.5, 0.000001, freeze_index=finish_index)
